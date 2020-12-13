9fccdc950c99b574e6e9e5f3cbece73133781c82
NathanZC <nathanccox52@gmail.com>
2020-12-13 12:33:58 -0500

    Changed attribute names. Cleaned code up for Python TA. Added Doctests.

--- dataformat.py
+++ dataformat.py
@@ -27,8 +27,8 @@ Instance Attributes
     name: str
     code: str
     population: int
-    GDP_yearly: Dict[int, float]
-    CO2_yearly: Dict[int, float]
+    gdp_yearly: Dict[int, float]
+    co2_yearly: Dict[int, float]
 
 
 def csv_to_dict(filepath: str) -> Dict[str, Dict[int, float]]:
@@ -37,39 +37,54 @@ def csv_to_dict(filepath: str) -> Dict[str, Dict[int, float]]:
     the first key is the country that gets mapped to a dict of years to information for that year
     """
     with open(filepath) as file:
-        reader = csv.reader(file)
-        next(reader)
-        data = {row[0]: {} for row in reader}
+        read = csv.reader(file)
+        next(read)
+        info = {}
+        for row in read:
+            info[row[0]] = {}
 
     with open(filepath) as file:
-        reader = csv.reader(file)
-        next(reader)
-        for row in reader:
+        read = csv.reader(file)
+        next(read)
+        for row in read:
             if 1990 <= int(row[2]) <= 2017:
-                data[row[0]][int(row[2])] = float(row[3])
-    return data
+                info[row[0]][int(row[2])] = float(row[3])
+    return info
 
 
-filepath = 'gdp-per-capita-worldbank.csv'
-with open(filepath) as file:
-    reader = csv.reader(file)
-    # Skip header row
-    next(reader)
-    country_codes = {row[0]: row[1] for row in reader}
-
-# Helper dicts
-gdp_data = csv_to_dict('gdp-per-capita-worldbank.csv')
-co2_data = csv_to_dict('annual-co2-emissions-per-country.csv')
-population_data = csv_to_dict('population-by-country.csv')
+def codes_to_dict(filepath: str) -> Dict[str, str]:
+    """
+    Changes the csv file data into a dict of dicts of data where
+    the countries are mapped the their corresponding codes
+    """
+    with open(filepath) as f:
+        reader = csv.reader(f)
+        # Skip header row
+        next(reader)
+        country_codes = {row[0]: row[1] for row in reader}
+    return country_codes
 
 
 def get_all_data() -> Dict[str, Country]:
     """
-    returns a dict with all of the data
+    returns a dict with all of the data.
     Compiles all smaller dicts into one mapping of the country to a object with the countries data.
-    NOTE: This dict will only contain countries that exist in both the gdp and co2 dataset.
-    This is the dataset to be used
+    >>> result = get_all_data()
+    >>> result['Canada'].gdp_yearly[2000]
+    37431.9169780243
+    >>> result['Canada'].code == 'CAN'
+    True
+    >>> result['Canada'].name == 'Canada'
+    True
+    >>> result['Canada'].co2_yearly[2000] == 571.5073457
+    True
     """
+    # Helper dicts
+    gdp_data = csv_to_dict('gdp-per-capita-worldbank.csv')
+    co2_data = csv_to_dict('annual-co2-emissions-per-country.csv')
+    population_data = csv_to_dict('population-by-country.csv')
+    country_codes = codes_to_dict('gdp-per-capita-worldbank.csv')
+    # main dict created using helpers
     all_data = {country_name:
                 Country(country_name,
                         country_codes[country_name],
@@ -80,8 +95,22 @@ def get_all_data() -> Dict[str, Country]:
                 if country_name in gdp_data and country_name in co2_data
                 and country_name in population_data and 2000 in population_data[country_name]
                 and country_codes[country_name] != ''
-                and all(i in gdp_data[country_name] and i in co2_data[country_name] for i in range(1990, 2018))}
+                and all(i in gdp_data[country_name] and i in co2_data[country_name]
+                        for i in range(1990, 2018))}
     return all_data
 
 
-data = get_all_data()
+if __name__ == '__main__':
+    import python_ta
+    python_ta.check_all(config={
+        'max-line-length': 100,
+        'extra-imports': ['python_ta.contracts', 'dataclasses', 'datetime', 'csv'],
+        'allowed-io': ['csv_to_dict', 'codes_to_dict']
+    })
+
+    import python_ta.contracts
+    python_ta.contracts.DEBUG_CONTRACTS = False
+    python_ta.contracts.check_all_contracts()
+
+    import doctest
+    doctest.testmod()
